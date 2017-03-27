8098bb75e2d8fde2c7327485033f8bcee376be43
raspi2 <raspi.2@innovalab.com>
2017-03-27 13:52:20 +0000

    .

--- scripts/bootStart.sh
+++ scripts/bootStart.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
 echo shell begin
-sleep 60
+sleep 40
 echo config GPIO pins
+sudo /usr/local/bin/gpio export 15 out
+sudo /usr/local/bin/gpio export 16 out
 sudo /usr/local/bin/gpio export 17 out
 sudo /usr/local/bin/gpio export 18 out
 echo launch program...
-cd /home/pi/Documents/ADAS-CPP/dist
-./ShapeColorDetector_v0.1
+cd /home/pi/Documents/ADAS-CPP
+./ShapeColorDetector.a
 
 
